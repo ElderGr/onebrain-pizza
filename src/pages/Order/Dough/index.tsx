1260e45888ee8cd04da3a14936c480cb77b47535
ElderGr <gbielrs@gmail.com>
2022-01-01 20:07:53 -0300

    feat: Implemented pizza dough selection with mocked values

--- src/pages/Order/Dough/index.tsx
+++ src/pages/Order/Dough/index.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// import { Container } from './styles';
+import { Container, DoughItem } from './styles';
+
+const doughs = [
+  {id: 1, name: 'Massa grossa', doughHeight: 7},
+  {id: 2, name: 'Massa média', doughHeight: 5},
+  {id: 3, name: 'Massa fina', doughHeight: 2}
+]
 
 const Dough: React.FC = () => {
+  
+  const [selectedDough, setSelectedDough] = useState<null | number>(null)
+
+  const handleSelectDough = (id: number) => {
+    setSelectedDough(id)
+  }
+
   return (
-      <div>
-          Dough
-      </div>
+      <Container>
+          <ul>
+            {doughs.map(dough => (
+              <DoughItem 
+                selected={selectedDough === dough.id}
+                onClick={() => handleSelectDough(dough.id)} 
+                doughHeight={dough.doughHeight}
+              >
+                <p>{dough.name}</p>
+                <span />
+              </DoughItem>
+            ))}
+          </ul>
+      </Container>
   );
 }
 
---
+++ src/pages/Order/Dough/styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+interface IDoughItem {
+    doughHeight: number;
+    selected: boolean;
+}
+
+export const Container = styled.div`
+    width: 40%;
+
+    >ul{
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        width: 450px;
+    }
+`
+
+export const DoughItem = styled.li<IDoughItem>`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 20px 0;
+
+    cursor: pointer;
+
+    >p {
+        font-size: 24pt;
+        font-weight:  ${props => `${props.doughHeight}00`};
+        color: ${props => props.selected ? '#23a723' : '#000'}
+    }
+
+    > span{
+        border-bottom: ${props => `${props.doughHeight}px`} solid ${props => props.selected ? '#23a723' : '#000'};
+        width: 150px;
+        height: 2px;
+    }
+`
