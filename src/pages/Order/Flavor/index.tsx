ee9879cbcbd176dcef4dc78491948fefe057a56d
ElderGr <gbielrs@gmail.com>
2022-01-01 18:42:07 -0300

    feat: Implemented pizza flavor selection with mocked values

--- src/pages/Order/Flavor/index.tsx
+++ src/pages/Order/Flavor/index.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// import { Container } from './styles';
+import { Container, FlavorItem } from './styles';
+
+const flavors = [
+  { id: 1 , name: 'marguerita', image:'../images/pizza2.png' },
+  { id: 2 , name: 'portuguesa', image:'../images/pizza4.png' },
+  { id: 3 , name: 'cogumelhos', image:'../images/pizza1.png' },
+  { id: 4 , name: 'festiva', image:'../images/pizza3.png' },
+  { id: 5 , name: 'rústica', image:'../images/pizza5.webp' }
+]
 
 const Flavor: React.FC = () => {
+  const [selectedFlavor, setSelectedFlavor] = useState<number | null>(null)
+
+  const handleFlavorSelect = (id: number) => {
+    setSelectedFlavor(id === selectedFlavor ? null : id)
+  }
+
   return (
-      <div>
-          Flavor
-      </div>
+      <Container>
+          <ul>
+            {flavors.map(flavor => (
+              <FlavorItem 
+                onClick={() => handleFlavorSelect(flavor.id)} 
+                image={flavor.image}
+                isSelected={selectedFlavor === flavor.id}
+              >
+                {flavor.name}
+              </FlavorItem>
+            ))}
+          </ul>
+      </Container>
   );
 }
 
---
+++ src/pages/Order/Flavor/styles.ts
@@ -0,0 +1,41 @@
+import styled, {css} from 'styled-components'
+
+interface IFlavorItem {
+    image: string;
+    isSelected: boolean;
+}
+
+export const Container = styled.div`
+    >ul{
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        
+        width: 650px;
+    }
+`
+
+export const FlavorItem = styled.li<IFlavorItem>`
+    height: 190px;
+    width: 170px;
+
+    display: flex;
+    justify-content: center;
+    
+    font-size: 16pt;
+    font-weight: 450;
+
+    margin: 10px;
+    ${props => css`
+        background: url(${props.image});
+    `}
+
+    ${props => css`
+        color: ${props.isSelected ? '#23a723' : '#000'};
+    `}
+
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: bottom;
+    cursor: pointer;
+`
