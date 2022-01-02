7998605d8e0fc05dd71bdcd91b3992b6ff72239d
ElderGr <gbielrs@gmail.com>
2022-01-02 14:04:00 -0300

    feat: implemented buyer data definition functionality

--- src/pages/Order/Data/index.tsx
+++ src/pages/Order/Data/index.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
-// import { Container } from './styles';
+import { Container, Input, Button } from './styles';
 
 const Data: React.FC = () => {
+  const [formValue, setFormValue] = useState({
+    name: '',
+    address: ''
+  })
+
+  const handleFormValue = useCallback((e) => {
+    setFormValue({
+      ...formValue,
+      [e.target.name]: e.target.value
+    })
+  }, [formValue])
+
+  const handleSubmit = useCallback(() => {
+    console.log(formValue)
+  }, [formValue])
+
   return (
-      <div>
-          Data
-      </div>
+      <Container>
+          <Input value={formValue.name} onChange={handleFormValue} placeholder='Nome Completo' />
+          <Input value={formValue.address} onChange={handleFormValue} placeholder='Endereço' />
+
+          <div>
+            <span>Preço: R$XX,XX</span>
+            <span>(pagar na entrada)</span>
+          </div>
+
+          <Button onClick={handleSubmit}>
+            Finalizar Pedido
+          </Button>
+      </Container>
   );
 }
 
---
+++ src/pages/Order/Data/styles.ts
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import { lighten } from 'polished'
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    width: 35%;
+
+    >div{
+        margin: 20px 0;
+        margin-bottom: 50px;
+        display: flex;
+        justify-content: space-between;
+
+        >span{
+            font-weight: 700;
+            font-size: 16pt;
+        }
+
+        >span:nth-child(2){
+            font-weight: 200;
+        }
+    }
+`
+
+export const Input = styled.input`
+    border: none;
+    border-bottom: 1px solid #858585;
+
+    font-size: 16pt;
+
+    padding: 10px 0;
+    margin: 10px 0;
+    background: none;
+
+    &::placeholder{
+        font-weight: 100;
+    }
+`
+
+export const Button = styled.button`
+    border: none;
+    color: #fff;
+    background: #000;
+
+    font-size: 16pt;
+
+    padding: 10px 0;
+    cursor: pointer;
+
+    transition: 0.2s;
+
+    &:hover{
+        background: ${() => lighten(0.15, '#000')}
+    }
+`
